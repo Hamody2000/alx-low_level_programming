e647efdbc33d4154fa007d7abddc89440d64a304
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-08-11 17:00:44 -0700

    Gafgaaga

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main -Entry level of the program
+ *
+ *Description: A function prints the number, and thenclassifies it
+ * as positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+	letter = "a";
+	while(letter <= "z")
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar("\n");
+	return (0);
+}
