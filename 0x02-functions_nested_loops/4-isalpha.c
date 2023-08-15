85b98e9add8b942b3bbbd7d3d121ef65161ba057
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-08-15 15:17:52 -0700

    is_alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - A function to check if the input is a character of not 
+ *
+ * @c: hte character to be checked
+ *
+ * Return: 1 if c is a letter; 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <=122 || c >=65 && c <=90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
