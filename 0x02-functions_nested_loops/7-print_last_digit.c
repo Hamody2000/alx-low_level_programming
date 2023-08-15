a656279bc3554a820c13f9d65fbc0996be78a4ef
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-08-15 15:46:28 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - it prints the last digit of the input
+ *
+ * @num: the input to be checked 
+ *
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int num)
+{
+	last_digit = num % 10;
+	_putchar('last_digit');
+
+	return (last_digit);
+}
