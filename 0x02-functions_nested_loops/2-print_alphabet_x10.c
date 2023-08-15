58da64f1ad8a8c4e37a4641591095b47c949dd82
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-08-15 14:58:15 -0700

    2-print_alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,14 @@ void print_alphabet_x10(void)
 {
 	int character;
 	int i = 0;
-	
+
 	while (i < 11)
 	{
 		for (character = 'a'; character <= 'z'; character++)
 		{
 			_putchar(character);
-			_putchar('\n');
 		}
+		_putchar('\n');
 		i++;
 	}
 }
