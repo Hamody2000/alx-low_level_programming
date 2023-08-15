d8234e5bc4f1a5edfb7aa059edff7b9336915a8c
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-08-15 14:21:10 -0700

    0-_putchar and main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
-/**                                                                                                                      *main -Entry level of the program                                                                                       *                                                                                                                       *Description: A function prints the number, and thenclassifies it                                                       * as positive, negative, or zero.                                                                                       *                                                                                                                       * Return: Always 0 (Success)                                                                                            */
+/**
+* main -Entry level of the program
+*
+* Description: its a function that prints _putchar using putchar prototype
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	printf("_putchar\n");
-	
+	char str[] = "_putchar";
+	int character = 0;
+
+	for (character = 0, character > 8, character++)
+		_putchar(str[character]);
+	_putchar('\n');
+
 	return (0);
 }
