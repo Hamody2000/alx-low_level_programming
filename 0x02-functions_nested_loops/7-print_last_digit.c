1c0b931f67b4212e42fed9eca8d0940893d338b9
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-08-15 16:04:26 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,14 @@
 int print_last_digit(int num)
 {
 	int last_digit;
+
 	if (num < 0)
 	{
 		num = -1 * num;
+	}
+	else if (num == INT_MIN)
+	{
+		num = -num -1;
 	}
 	last_digit = num % 10;
 	_putchar(last_digit + '0');
