3557a12eddc36b65b4b0e6db419005f4bbd30095
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-08-15 15:09:18 -0700

    is lowecase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,15 @@
 
 /**
  * _islower - its a function that return 1 if c is lowecase
+ *
+ * @c - is the character the user puts to see if its a lowercase or not
+ *
+ * Return - 1 if its a lowercase character; 0 if its not
  */
 
 int _islower(int c)
 {
-	if ( c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
