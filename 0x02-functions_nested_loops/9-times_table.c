86377b7e6849fc6088c1dedbd50770f349f06802
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-08-15 17:57:34 -0700

    last comment

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,6 @@ void times_table(void)
 
 	while (i < 10)
 	{
-		_putchar(48);
 		for (j = 0; j <= 9; j++)
 		{
 			_putchar(',');
@@ -22,12 +21,12 @@ void times_table(void)
 
 			res = i * j;
 
-			if (res <=9)
+			if (res <= 9)
 				_putchar(' ');
 			else
 				_putchar((res / 10) + 48);
 
-			_putchar((res % 10) +48);
+			_putchar((res % 10) + 48);
 		}
 		_putchar('\n');
 		i++;
