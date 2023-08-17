0fcf3e168c0af50b2afa78be329252039b1b2a55
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-08-17 16:35:11 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - a function check for upper letter @a and @b
+* @a: a character to be checked
+* @b: also checked
+* Return: 0 if not amd 1 if upper
+*/
+
+int mul(int a, int b)
+{
+	res = a * b;
+	_putchar("res");
+}
