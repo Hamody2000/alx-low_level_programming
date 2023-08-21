1c90142e60954aba0450c85410cef4effd770ee7
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-08-21 16:52:18 -0700

    lol

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,20 @@
 #include "main.h"
 
 /**
-* int _strlen - a function check for upper letter
+* _strlen - a function check for upper letter
 * @s: a character to be checked
-* Return: 0 if not amd 1 if upper
+* Return: lenght if not amd 1 if upper
 */
 
-int _strlen(char *s);
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (*s != '\0')
+	{
+		lenght++;
+		s++;
+	}
+
+	return lenght;
+}
