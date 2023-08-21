95493954eb7c19ec51dd21941ed686e677de3788
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-08-21 16:28:39 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - a function check for upper letter
+* @*n: a character to be checked
+* Return: 0 if not amd 1 if upper
+*/
+
+void reset_to_98(int *n)
+{
+	n = 98;
+}
