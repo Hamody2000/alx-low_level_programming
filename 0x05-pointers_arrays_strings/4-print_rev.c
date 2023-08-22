d0c1c1e1aa95f8d0782c2f0be4f03d8c8b7e0870
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-08-21 17:11:13 -0700

    lolllo

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,23 @@
 #include "main.h"
 
 /**
-* int print_rev - a function check for upper letter
+* print_rev - a function check for upper letter
 * @s: a character to be checked
 * Return: 0 if not amd 1 if upper
 */
 
 void print_rev(char *s)
+{
+	int lenght = _strlen(*s);
+	int temp;
+
+	while (i < lenght)
+	{
+		temp = *s[lenght];
+		*s[lenght] = *s[i];
+		*s[i] = temp;
+		i++;
+		lenght--;
+	}
+	_puts(*s);
+}
