72c2812e793c4c0be6ce1a1470208c9a5e03b6b9
Hamody2000 <ahmed.hamody5532@gmail.com>
2023-10-25 15:47:00 -0700

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,31 @@
+#include "main.h"
 
+/**
+ *  _sqrt_recursion - a function put a string
+ * @n: the output
+ * @val: square root jgf
+ * Return: a number
+ */
+
+int square(int n, int val);
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * square - a function
+ * @n: numbering
+ * @val: valuinghu
+ * Return: integer
+ */
+
+int square(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (square(n, val +1));
+	else
+		return (-1);
+}
