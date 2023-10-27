2c40c26f270f63d3e68985e062473bbd032c405d
Hamody2000 <you@example.com>
2023-10-28 01:48:16 +0200

    second task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,9 @@
  * Return: 0
  */
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    (void)argc;
-    printf("%d\n", argc - 1);
-    return (0);
+	(void)argc;
+	printf("%d\n", argc - 1);
+	return (0);
 }
