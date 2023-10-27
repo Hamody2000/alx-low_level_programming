f74d8fd2b8185de1bdf7fcebc3d4a31576439e6c
Hamody2000 <you@example.com>
2023-10-28 01:44:47 +0200

    second task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print name
+ * @argc: number
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+    (void)argc;
+    printf("%d\n", argc - 1);
+    return (0);
+}
